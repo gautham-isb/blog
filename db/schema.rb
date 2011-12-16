317e4e75703f24ede47f44273813a8f11fc1a0d4
Gautham <gautham@integralsense.com>
2011-12-15 23:29:00 -0800

    Finished Using Associations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111216013351) do
+ActiveRecord::Schema.define(:version => 20111216072053) do
 
   create_table "articles", :force => true do |t|
     t.string   "title"
@@ -31,6 +31,15 @@ ActiveRecord::Schema.define(:version => 20111216013351) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "comments", :force => true do |t|
+    t.integer  "article_id"
+    t.string   "name"
+    t.string   "email"
+    t.text     "body"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
