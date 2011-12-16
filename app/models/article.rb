e7902d85cf87694d3409152e4a46f7dc7da082b4
Gautham Nookala <gautham@integralsense.com>
2011-12-16 14:12:40 -0800

    Finished Advanced Finding:

--- app/models/article.rb
+++ app/models/article.rb
@@ -6,8 +6,16 @@ class Article < ActiveRecord::Base
 	has_and_belongs_to_many :categories
 	has_many :comments
 	
+	scope :published, where("articles.published_at IS NOT NULL")
+	scope :draft, where("articles.published_at IS NULL")
+	
+	scope :recent, lambda { published.where("articles.published_at > 
+	?", 1.week.ago.to_date) }
+	
+	scope :where_title, lambda { |term| where("articles.title LIKE ?", "%#{term}%") }
+	
 	def long_title
-		"#{title} - #{published_at}"
+		"#{title} - #{published_at.to_date}"
 	end
 	
 	
