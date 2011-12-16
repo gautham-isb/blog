84868b84ce4f073da147fee76715987df01c9f58
Gautham Nookala <gautham@integralsense.com>
2011-12-15 17:42:02 -0800

    Many to Many Association

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,9 @@ class Article < ActiveRecord::Base
 	validates :title, :presence => true
 	validates :body, :presence => true
 	
+	belongs_to :user
+	has_and_belongs_to_many :categories
+	
 	def long_title
 		"#{title} - #{published_at}"
 	end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111216010943) do
+ActiveRecord::Schema.define(:version => 20111216013351) do
 
   create_table "articles", :force => true do |t|
     t.string   "title"
@@ -21,6 +21,36 @@ ActiveRecord::Schema.define(:version => 20111216010943) do
     t.datetime "updated_at"
     t.string   "excerpt"
     t.string   "location"
+    t.integer  "user_id"
+  end
+
+  create_table "articles_categories", :id => false, :force => true do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+  end
+
+  create_table "categories", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "profiles", :force => true do |t|
+    t.integer  "user_id"
+    t.string   "name"
+    t.date     "birthday"
+    t.text     "bio"
+    t.string   "color"
+    t.string   "twitter"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "email"
+    t.string   "password"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
