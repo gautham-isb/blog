42694ff919a125d19cbc432c6e741ed55c85ba38
Gautham Nookala <gautham@integralsense.com>
2011-12-20 14:23:58 -0800

    Added Comment Observer

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,13 +4,8 @@ class Comment < ActiveRecord::Base
   validates :name, :email, :body, :presence => true
   validate :article_should_be_published
   
-  before_create :email_article_author
-
   def article_should_be_published
     errors.add(:article_id, "is not published yet") if article && !article.published?
   end
   
-  def email_article_author
-  	puts "We will notify #{article.user.email} in Chapter 9"
-  end
 end
---
+++ app/models/comment_observer.rb
@@ -0,0 +1,6 @@
+class CommentObserver < ActiveRecord::Observer
+	def after_create(comment)
+		puts "We will notify the author in Chapter 9"
+	end
+	
+end
