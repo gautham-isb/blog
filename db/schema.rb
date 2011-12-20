75e2878a53a6da4e5214544f0615a4957464ac44
Gautham Nookala <gautham@integralsense.com>
2011-12-20 15:21:35 -0800

    add hased passwords

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111216072053) do
+ActiveRecord::Schema.define(:version => 20111220222926) do
 
   create_table "articles", :force => true do |t|
     t.string   "title"
@@ -57,7 +57,7 @@ ActiveRecord::Schema.define(:version => 20111216072053) do
 
   create_table "users", :force => true do |t|
     t.string   "email"
-    t.string   "password"
+    t.string   "hashed_password"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
